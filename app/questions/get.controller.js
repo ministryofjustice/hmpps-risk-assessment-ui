3593326b4521af4466f8a2c60adf6efac17dd103
Jez Higgins <jez@jezuk.co.uk>
2020-10-20 16:22:07 +0100

    List toplevel forms

--- app/questions/get.controller.js
+++ app/questions/get.controller.js
@@ -5,7 +5,23 @@ const displayQuestionList = async ({ tokens }, res) => {
   try {
     const questionsList = await getQuestionList(tokens)
 
-    const topLevelForms = questionsList.filter(form => form.questionCount === 0)
+    const topLevelForms = questionsList
+      .filter(form => form.questionCount === 0)
+      .map(form => {
+        return {
+          ...form,
+          path: `/questionGroup/${form.groupId}`,
+        }
+      })
+
+    topLevelForms.sort((l, r) => {
+      const lTitle = l.title.toUpperCase()
+      const rTitle = r.title.toUpperCase()
+
+      if (lTitle < rTitle) return -1
+      if (lTitle > rTitle) return 1
+      return 0
+    })
 
     return res.render(`${__dirname}/index`, {
       forms: topLevelForms,
