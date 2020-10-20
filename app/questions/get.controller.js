36efc5c7435c020d84852a6a3d51f352c93422e7
Jez Higgins <jez@jezuk.co.uk>
2020-10-20 15:58:45 +0100

    Hook up to question list endpoint

--- app/questions/get.controller.js
+++ app/questions/get.controller.js
@@ -1,5 +1,19 @@
+const { logger } = require('../../common/logging/logger')
+const { getQuestionList } = require('../../common/data/assessmentApi')
+
 const displayQuestionList = async ({ tokens }, res) => {
-  return res.render(`${__dirname}/index`, {})
+  try {
+    const questionsList = await getQuestionList(tokens)
+
+    const topLevelForms = questionsList.filter(form => form.questionCount === 0)
+
+    return res.render(`${__dirname}/index`, {
+      forms: topLevelForms,
+    })
+  } catch (error) {
+    logger.error(`Could not retrieve questions list, error: ${error}`)
+    return res.render('app/error', { error })
+  }
 }
 
 module.exports = { displayQuestionList }
