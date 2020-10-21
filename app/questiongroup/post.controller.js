113f1db9c440eda94e1656860b3600c6809c2475
Jez Higgins <jez@jezuk.co.uk>
2020-10-21 10:31:20 +0100

    Hook up post route, so we can start work on saving data

---
+++ app/questiongroup/post.controller.js
@@ -0,0 +1,24 @@
+const { logger } = require('../../common/logging/logger')
+const { getQuestionGroup } = require('../../common/data/assessmentApi')
+
+const assessmentId = 'e69a61ff-7395-4a12-b434-b1aa6478aded'
+const episodeId = 'e69a61ff-7395-4a12-b434-b1aa6478aded'
+
+const saveQuestionGroup = async ({ params: { groupId }, body, tokens }, res) => {
+  try {
+    console.log(body)
+
+    const questionGroup = await getQuestionGroup(groupId, tokens)
+
+    return res.render(`${__dirname}/index`, {
+      heading: questionGroup.contents[0].title,
+      groupId,
+      questions: questionGroup.contents[0].contents,
+    })
+  } catch (error) {
+    logger.error(`Could not retrieve question group for ${groupId}, error: ${error}`)
+    return res.render('app/error', { error })
+  }
+}
+
+module.exports = { saveQuestionGroup }
