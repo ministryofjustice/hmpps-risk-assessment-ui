0ad017267389929ddb5c999cbe42e254e02d36df
Paul Coupe <paul.coupe@digital.justice.gov.uk>
2020-11-24 06:58:37 +0000

    ARN-91 linting

--- integration-tests/plugins/index.js
+++ integration-tests/plugins/index.js
@@ -4,13 +4,15 @@ const { resetStubs } = require('../../wiremock/wiremock')
 const { stubForms, stubQuestions, stubAnswers, stubEpisodes } = require('../../wiremock/assessmentApi')
 const oauthApi = require('../../wiremock/oauth')
 
-module.exports = (on, config) => {
+module.exports = on => {
+  // eslint-disable-next-line no-unused-vars
   on('before:browser:launch', (browser = {}, launchOptions) => {
     prepareAudit(launchOptions)
   })
 
   on('task', {
     lighthouse: lighthouse(lighthouseReport => {
+      // eslint-disable-next-line no-console
       console.log(lighthouseReport) // raw lighthouse report
     }),
     pa11y: pa11y(), // calling the function is important
