1b57735a9e27f58f4dce1789d0fa0be8ce9ae8ff
Paul Coupe <paul.coupe@digital.justice.gov.uk>
2020-12-14 17:39:48 +0000

    ARN-143 display offender details

---
+++ common/middleware/getOffenderDetails.js
@@ -0,0 +1,20 @@
+const logger = require('../logging/logger')
+const { getOffenderData } = require('../data/assessmentApi')
+
+module.exports = async ({ tokens }, res, next) => {
+  try {
+    const { name, pnc = null, crn = null, dob, age } = await getOffenderData(tokens)
+    if (!name) throw new Error('Required offender details could not be found')
+    res.locals.offenderDetails = {
+      name,
+      crn,
+      pnc,
+      age,
+      dob,
+    }
+    return next()
+  } catch (error) {
+    logger.error(`Could not retrieve offender details, error: ${error}`)
+    return res.render('app/error', { error })
+  }
+}
