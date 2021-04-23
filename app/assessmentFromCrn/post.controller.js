f0fe74cf09c3d88e4c5a6e89e20961faa355a44d
Stuart Whitlam <stuart.whitlam@digital.justice.gov.uk>
2021-04-23 13:19:17 +0100

    Update create assessment endpoint Add create assessment from CRN helper page

---
+++ app/assessmentFromCrn/post.controller.js
@@ -0,0 +1,27 @@
+// const { logger } = require('../../common/logging/logger')
+const { assessmentSupervision } = require('../../common/data/hmppsAssessmentApi')
+
+const startAssessmentFromCrn = ({ params: { crn, deliusEventId, assessmentType }, tokens }, res) => {
+  return startAssessment(crn, deliusEventId, assessmentType, tokens, res)
+}
+
+const startAssessmentFromForm = ({ body, tokens }, res) => {
+  const { crn, deliusEventId, assessmentType } = body
+  return startAssessment( crn, deliusEventId, assessmentType, tokens, res)
+}
+
+const startAssessment = async ( crn, deliusEventId, assessmentType, tokens, res) => {
+  try {
+    // eslint-disable-next-line no-unused-vars
+    const [ok, assessment] = await assessmentSupervision({ crn, deliusEventId, assessmentType }, tokens)
+    console.log(assessment.assessmentUuid)
+    return res.redirect(`/${assessment.assessmentUuid}/questionGroup/pre_sentence_assessment/summary`)
+  } catch (error) {
+    return res.render('app/error', { error })
+  }
+}
+
+module.exports = {
+  startAssessmentFromCrn,
+  startAssessmentFromForm,
+}
