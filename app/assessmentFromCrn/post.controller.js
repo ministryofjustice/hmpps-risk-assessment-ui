ddc5aac4c25053679c15eeb0701d66b79ffc2031
Stuart Whitlam <stuart.whitlam@digital.justice.gov.uk>
2021-04-23 13:23:37 +0100

    Linting

--- app/assessmentFromCrn/post.controller.js
+++ app/assessmentFromCrn/post.controller.js
@@ -7,14 +7,13 @@ const startAssessmentFromCrn = ({ params: { crn, deliusEventId, assessmentType }
 
 const startAssessmentFromForm = ({ body, tokens }, res) => {
   const { crn, deliusEventId, assessmentType } = body
-  return startAssessment( crn, deliusEventId, assessmentType, tokens, res)
+  return startAssessment(crn, deliusEventId, assessmentType, tokens, res)
 }
 
-const startAssessment = async ( crn, deliusEventId, assessmentType, tokens, res) => {
+const startAssessment = async (crn, deliusEventId, assessmentType, tokens, res) => {
   try {
     // eslint-disable-next-line no-unused-vars
     const [ok, assessment] = await assessmentSupervision({ crn, deliusEventId, assessmentType }, tokens)
-    console.log(assessment.assessmentUuid)
     return res.redirect(`/${assessment.assessmentUuid}/questionGroup/pre_sentence_assessment/summary`)
   } catch (error) {
     return res.render('app/error', { error })
