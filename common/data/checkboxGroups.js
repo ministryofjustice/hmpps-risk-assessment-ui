03a1566709e1164dde9051429feaddeec4fac593
Lucas Cairns <lucas.cairns@gmail.com>
2021-07-27 12:02:06 +0100

    Add initial code to extract checkbox group answers

--- common/data/checkboxGroups.js
+++ common/data/checkboxGroups.js
@@ -22,6 +22,30 @@ const flattenCheckboxGroups = questions => {
   })
 }
 
+const extractCheckboxGroupAnswers = (questions, answers) => {
+  const checkboxGroups = questions.filter(question => question.type === 'checkboxGroup')
+
+  const extractedAnswers = checkboxGroups.reduce(
+    (previousAnswers, checkboxGroup) => {
+      const updatedAnswers = { ...previousAnswers }
+      const checkboxGroupQuestions = checkboxGroup.contents
+      checkboxGroupQuestions.forEach(({ questionId, answerSchemas = [] }) => {
+        const answersForThisGroup = updatedAnswers[checkboxGroup.checkboxGroupId] || []
+        const [firstAnswer, secondAnswer] = answerSchemas
+        updatedAnswers[questionId] = answersForThisGroup.includes(questionId) ? firstAnswer?.value : secondAnswer?.value
+      })
+      return updatedAnswers
+    },
+    { ...answers },
+  )
+
+  // Tidy up the answers and remove the checkBox group answers
+  checkboxGroups.forEach(({ checkboxGroupId }) => delete extractedAnswers[checkboxGroupId])
+
+  return extractedAnswers
+}
+
 module.exports = {
   flattenCheckboxGroups,
+  extractCheckboxGroupAnswers,
 }
--- common/data/checkboxGroups.test.js
+++ common/data/checkboxGroups.test.js
@@ -1,69 +1,69 @@
-const { flattenCheckboxGroups } = require('./checkboxGroups')
+const { flattenCheckboxGroups, extractCheckboxGroupAnswers } = require('./checkboxGroups')
 
 describe('checkboxGroups', () => {
-  describe('flattenCheckboxGroups', () => {
-    const checkboxGroup = {
-      type: 'checkboxGroup',
-      checkboxGroupId: '91a60f48-89d4-4106-8f8a-fe797edca111',
-      checkboxGroupCode: 'checkbox_group',
-      title: 'Checkbox group',
-      displayOrder: 1,
-      contents: [
-        {
-          type: 'question',
-          questionId: '0941c5b2-f42d-4120-ad79-44954674fe00',
-          questionCode: '1.1',
-          answerType: 'checkbox',
-          questionText: 'First option',
-          displayOrder: 1,
-          mandatory: true,
-          readOnly: false,
-          conditional: false,
-          referenceDataTargets: [],
-          answerSchemas: [
-            {
-              answerSchemaUuid: '59a0f4fe-4cca-426b-9402-0236dae24902',
-              answerSchemaCode: 'yes',
-              value: 'YES',
-              text: 'Yes',
-            },
-            {
-              answerSchemaUuid: 'c36d2ccd-c049-4640-806e-34b012f682d8',
-              answerSchemaCode: 'no',
-              value: 'NO',
-              text: 'No',
-            },
-          ],
-        },
-        {
-          type: 'question',
-          questionId: 'f988f76c-3d6c-4f45-aa29-7dc8d11198d7',
-          questionCode: '1.2',
-          answerType: 'checkbox',
-          questionText: 'Second option',
-          displayOrder: 2,
-          mandatory: true,
-          readOnly: false,
-          conditional: false,
-          referenceDataTargets: [],
-          answerSchemas: [
-            {
-              answerSchemaUuid: '59a0f4fe-4cca-426b-9402-0236dae24902',
-              answerSchemaCode: 'yes',
-              value: 'YES',
-              text: 'Yes',
-            },
-            {
-              answerSchemaUuid: 'c36d2ccd-c049-4640-806e-34b012f682d8',
-              answerSchemaCode: 'no',
-              value: 'NO',
-              text: 'No',
-            },
-          ],
-        },
-      ],
-    }
+  const checkboxGroup = {
+    type: 'checkboxGroup',
+    checkboxGroupId: '91a60f48-89d4-4106-8f8a-fe797edca111',
+    checkboxGroupCode: 'checkbox_group',
+    title: 'Checkbox group',
+    displayOrder: 1,
+    contents: [
+      {
+        type: 'question',
+        questionId: '0941c5b2-f42d-4120-ad79-44954674fe00',
+        questionCode: '1.1',
+        answerType: 'checkbox',
+        questionText: 'First option',
+        displayOrder: 1,
+        mandatory: true,
+        readOnly: false,
+        conditional: false,
+        referenceDataTargets: [],
+        answerSchemas: [
+          {
+            answerSchemaUuid: '59a0f4fe-4cca-426b-9402-0236dae24902',
+            answerSchemaCode: 'yes',
+            value: 'YES',
+            text: 'Yes',
+          },
+          {
+            answerSchemaUuid: 'c36d2ccd-c049-4640-806e-34b012f682d8',
+            answerSchemaCode: 'no',
+            value: 'NO',
+            text: 'No',
+          },
+        ],
+      },
+      {
+        type: 'question',
+        questionId: 'f988f76c-3d6c-4f45-aa29-7dc8d11198d7',
+        questionCode: '1.2',
+        answerType: 'checkbox',
+        questionText: 'Second option',
+        displayOrder: 2,
+        mandatory: true,
+        readOnly: false,
+        conditional: false,
+        referenceDataTargets: [],
+        answerSchemas: [
+          {
+            answerSchemaUuid: '59a0f4fe-4cca-426b-9402-0236dae24902',
+            answerSchemaCode: 'yes',
+            value: 'YES',
+            text: 'Yes',
+          },
+          {
+            answerSchemaUuid: 'c36d2ccd-c049-4640-806e-34b012f682d8',
+            answerSchemaCode: 'no',
+            value: 'NO',
+            text: 'No',
+          },
+        ],
+      },
+    ],
+  }
 
+  describe('flattenCheckboxGroups', () => {
     it('flattens a checkbox group in to a single question', () => {
       const questions = flattenCheckboxGroups([checkboxGroup])
       expect(questions).toEqual([
@@ -83,6 +83,23 @@ describe('checkboxGroups', () => {
           ],
         },
       ])
+    })
+  })
+
+  describe('extractCheckboxGroupAnswers', () => {
+    it('extracts checkbox group answers', () => {
+      const answers = {
+        '91a60f48-89d4-4106-8f8a-fe797edca111': [
+          '0941c5b2-f42d-4120-ad79-44954674fe00',
+          'f988f76c-3d6c-4f45-aa29-7dc8d11198d7',
+        ],
+      }
+      const extractedAnswers = extractCheckboxGroupAnswers([checkboxGroup], answers)
+
+      expect(extractedAnswers).toEqual({
+        '0941c5b2-f42d-4120-ad79-44954674fe00': 'YES',
+        'f988f76c-3d6c-4f45-aa29-7dc8d11198d7': 'YES',
+      })
     })
   })
 })
