5605fa7778c38ff153dff18e518cdd95ee82a6e7
Lucas Cairns <lucas.cairns@gmail.com>
2021-09-20 16:15:16 +0100

    Tidy up start controller

--- app/rsr/controllers/start.js
+++ app/rsr/controllers/start.js
@@ -1,10 +1,6 @@
 const { Controller } = require('hmpo-form-wizard')
 
 class StartRsr extends Controller {
-  async getValues(req, res, next) {
-    super.getValues(req, res, next)
-  }
-
   locals(req, res, next) {
     res.locals.csrfToken = res.locals['csrf-token'] // TODO: move this to a BaseController class
     res.locals.assessment = req.session.assessment
