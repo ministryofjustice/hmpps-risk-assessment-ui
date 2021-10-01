95d3e69d5416d5caa8658586af2c6261c26e9c2f
Lucas Cairns <lucas.cairns@gmail.com>
2021-10-01 09:49:15 +0100

    Refactor Save and Continue controller

--- app/rsr/controllers/saveAndContinue.js
+++ app/rsr/controllers/saveAndContinue.js
@@ -1,9 +1,9 @@
 const { Controller } = require('hmpo-form-wizard')
-const { postAnswers } = require('../../../common/data/hmppsAssessmentApi')
-const { formatValidationErrors, assembleDates } = require('../../../common/middleware/questionGroups/postHandlers')
+const nunjucks = require('nunjucks')
+const { postAnswers, getFlatAssessmentQuestions } = require('../../../common/data/hmppsAssessmentApi')
 const { logger } = require('../../../common/logging/logger')
 const { customValidations } = require('../fields')
-const getAssessmentQuestions = require('../../../common/middleware/getAssessmentQuestions')
+const { processReplacements } = require('../../../common/utils/util')
 
 const getErrorMessage = reason => {
   if (reason === 'OASYS_PERMISSION') {
@@ -13,52 +13,273 @@ const getErrorMessage = reason => {
   return 'Something went wrong'
 }
 
+const pageValidationErrorsFrom = (validationErrors, serverErrors = []) => {
+  const errors = Object.values(validationErrors).reduce(
+    (otherErrors, currentError) => ({
+      validationErrors: { ...otherErrors.validationErrors, [currentError.key]: { text: currentError.message } },
+      errorSummary: [
+        ...otherErrors.errorSummary,
+        {
+          text: currentError.message,
+          href: `#${[currentError.key]}-error`,
+        },
+      ],
+    }),
+    { validationErrors: {}, errorSummary: [] },
+  )
+
+  const processedServerErrors = serverErrors.map(errorMessage => ({
+    text: errorMessage,
+    href: `#`,
+  }))
+
+  return {
+    validationErrors: errors.validationErrors,
+    errorSummary: [...processedServerErrors, ...errors.errorSummary],
+  }
+}
+
+const withAnswersFrom = answers => ([fieldName, fieldProperties]) => {
+  if (fieldProperties.answerType === 'radio') {
+    const checkedAnswer = answers[fieldName]
+    return {
+      ...fieldProperties,
+      answerSchemas: fieldProperties.answerSchemas.map(answerSchema => ({
+        ...answerSchema,
+        checked: checkedAnswer === answerSchema.value,
+      })),
+    }
+  }
+
+  if (fieldProperties.answerType === 'checkbox') {
+    const selected = answers[fieldName] || []
+    return {
+      ...fieldProperties,
+      answerSchemas: fieldProperties.answerSchemas.map(answerSchema => ({
+        ...answerSchema,
+        selected: selected.includes(answerSchema.value),
+      })),
+    }
+  }
+
+  const answer = answers[fieldName] || ''
+
+  return {
+    ...fieldProperties,
+    answer,
+  }
+}
+
+const fieldFrom = (localField, questionSchemaDto = {}) => {
+  const validationRules = [...localField.validate]
+  if (
+    questionSchemaDto.mandatory &&
+    localField.validate.filter(validationRule => validationRule.type === 'required').length === 0
+  ) {
+    const remoteValidationRules = questionSchemaDto.validation ? JSON.parse(questionSchemaDto.validation) : {}
+    const { mandatory = {} } = remoteValidationRules
+    validationRules.push({
+      type: 'required',
+      message: mandatory.errorMessage || `[PLACEHOLDER] ${questionSchemaDto.questionText} is mandatory`,
+    })
+  }
+
+  return {
+    conditional: questionSchemaDto.conditional || false,
+    readOnly: questionSchemaDto.readOnly || false,
+    questionCode: questionSchemaDto.questionCode,
+    questionText: questionSchemaDto.questionText,
+    helpText: questionSchemaDto.helpText,
+    referenceDataTargets: questionSchemaDto.referenceDataTargets,
+    answerType: questionSchemaDto.answerType,
+    answerSchemas: questionSchemaDto.answerSchemas,
+    validate: validationRules,
+    dependent: localField.dependent,
+    answer: '',
+  }
+}
+
+const keysByQuestionCode = (otherQuestions, currentQuestion) => ({
+  ...otherQuestions,
+  [currentQuestion.questionCode]: currentQuestion,
+})
+
+const combinedLocalFieldsWith = remoteQuestions => (otherFields, [questionCode, localQuestion]) => ({
+  ...otherFields,
+  [questionCode]: fieldFrom(localQuestion, remoteQuestions[questionCode]),
+})
+
+const combineDateFields = (formValues = {}) => {
+  const dateFieldPattern = /-(day|month|year)$/
+  const whereDateField = key => dateFieldPattern.test(key)
+
+  const dateFieldNames = Object.keys(formValues).filter(whereDateField)
+  const nonDateFieldNames = Object.keys(formValues).filter(fieldName => !whereDateField(fieldName))
+
+  const combinedDateFieldsFor = answers => (otherFields, fieldName) => {
+    const dateKey = fieldName.replace(dateFieldPattern, '')
+
+    if (answers[`${dateKey}-year`] === '' || answers[`${dateKey}-month`] === '' || answers[`${dateKey}-day`] === '') {
+      return { ...otherFields, [dateKey]: '' }
+    }
+
+    const year = answers[`${dateKey}-year`]
+    const month = answers[`${dateKey}-month`].toString().padStart(2, '0')
+    const day = answers[`${dateKey}-day`].toString().padStart(2, '0')
+
+    return { ...otherFields, [dateKey]: `${year}-${month}-${day}` }
+  }
+
+  const answersFrom = answers => (otherFields, fieldName) => ({ ...otherFields, [fieldName]: answers[fieldName] })
+
+  const combinedDateFields = dateFieldNames.reduce(combinedDateFieldsFor(formValues), {})
+  return nonDateFieldNames.reduce(answersFrom(formValues), combinedDateFields)
+}
+
+const answerDtoFrom = formValues =>
+  Object.keys(formValues)
+    .filter(fieldName => formValues[fieldName] && formValues[fieldName] !== '')
+    .reduce(
+      (otherFields, fieldName) => ({
+        ...otherFields,
+        [fieldName]: Array.isArray(formValues[fieldName]) ? formValues[fieldName] : [formValues[fieldName]],
+      }),
+      {},
+    )
+
+const renderConditionalQuestion = (conditionalQuestions, errors, _nunjucks = nunjucks) => answerSchema => {
+  const questionsDependentOnThisAnswer = conditionalQuestions.filter(
+    question => question.dependent.value === answerSchema.value,
+  )
+
+  const rendered = questionsDependentOnThisAnswer.reduce((previouslyRendered, conditionalQuestion) => {
+    const validationError = errors[conditionalQuestion.questionCode]
+
+    const questionString = JSON.stringify(conditionalQuestion)
+
+    const errorString = validationError ? `, ${JSON.stringify(validationError)}` : ''
+
+    const conditionalQuestionString = `
+          {% from "common/templates/components/question/macro.njk" import renderQuestion %}
+          {{ renderQuestion(${questionString},'',''${errorString}) }}
+        `
+
+    const renderedQuestion = _nunjucks.renderString(conditionalQuestionString).replace(/(\r\n|\n|\r)\s+/gm, '')
+
+    return [previouslyRendered, renderedQuestion].join('')
+  }, '')
+  return { ...answerSchema, conditional: { html: rendered } }
+}
+
+const compileConditionalQuestions = (questions, errors) => {
+  const inlineConditionalQuestions = questions.filter(
+    question => question.dependent && !question.dependent.displayOutOfLine,
+  )
+
+  const groupedConditionalQuestions = inlineConditionalQuestions.reduce(
+    (groups, currentQuestion) => ({
+      ...groups,
+      [currentQuestion.dependent.field]: [
+        ...(groups[currentQuestion.dependent.field] || []),
+        currentQuestion.questionCode,
+      ],
+    }),
+    {},
+  )
+
+  return Object.entries(groupedConditionalQuestions).reduce(
+    (otherQuestions, [questionCode, conditionalQuestionCodes]) => {
+      const [questionSchema] = otherQuestions.filter(q => q.questionCode === questionCode)
+      const conditionalQuestionSchemas = otherQuestions.filter(q => conditionalQuestionCodes.includes(q.questionCode))
+
+      const updatedAnswerSchemas = questionSchema.answerSchemas.map(
+        renderConditionalQuestion(conditionalQuestionSchemas, errors),
+      )
+
+      return [...otherQuestions, { ...questionSchema, answerSchemas: updatedAnswerSchemas }]
+    },
+    [...questions],
+  )
+}
+
 class SaveAndContinue extends Controller {
-  // GET steps
   async locals(req, res, next) {
     res.locals.csrfToken = res.locals['csrf-token']
     delete res.locals['csrf-token']
 
-    // get questions
-    await getAssessmentQuestions(req, res, next)
     res.locals.assessment = req.session.assessment || {}
+
+    const errors = req.sessionModel.get('errors') || {}
+    const { validationErrors, errorSummary } = pageValidationErrorsFrom(errors)
+    res.locals.errors = validationErrors
+    res.locals.errorSummary = errorSummary
+
+    const submittedAnswers = req.sessionModel.get('answers') || {}
+    const questions = Object.entries(req.form.options.fields)
+    const questionsWithMappedAnswers = questions.map(withAnswersFrom(submittedAnswers))
+    const questionWithPreCompiledConditionals = compileConditionalQuestions(
+      questionsWithMappedAnswers,
+      validationErrors,
+    )
+    // const questionsWithCompiledConditionals = compileInlineConditionalQuestions(questionsWithMappedAnswers, res.locals.errors)
+    const questionsWithReplacements = processReplacements(
+      questionWithPreCompiledConditionals,
+      req.session?.assessment?.subject,
+    )
+
+    res.locals.questions = questionsWithReplacements.reduce(keysByQuestionCode, {})
 
     super.locals(req, res, next)
   }
 
   // POST steps
   async configure(req, res, next) {
-    await assembleDates(req, res, () => {})
+    const combineLocalAndRemoteFields = (fields, remoteFields) =>
+      Object.entries(fields).reduce(combinedLocalFieldsWith(remoteFields), {})
+
+    const questions = await getFlatAssessmentQuestions('RSR', req.user?.token, req.user?.id)
+    const questionMap = questions.reduce(keysByQuestionCode, {})
+    req.form.options.allFields = combineLocalAndRemoteFields(req.form.options.allFields, questionMap)
+    req.form.options.fields = combineLocalAndRemoteFields(req.form.options.fields, questionMap)
+
     super.configure(req, res, next)
   }
 
   process(req, res, next) {
-    req.sessionModel.set('answers', req.form.values)
+    const withValuesFrom = answers => (otherAnswers, currentField) => ({
+      ...otherAnswers,
+      [currentField]: answers[currentField] || '',
+    })
+    const filterAnswersForFields = (fields, answers) => Object.keys(fields).reduce(withValuesFrom(answers), {})
+
+    const requestBody = req.body || {}
+    const answersWithFormattedDates = combineDateFields(requestBody)
+    req.form.values = filterAnswersForFields(req.form?.options?.fields, answersWithFormattedDates)
+    req.sessionModel.set('answers', req.form?.values || {})
     super.process(req, res, next)
   }
 
   async validateFields(req, res, next) {
     // at this point makes changes to sessionModel fields to add in context specific validations
-    const { date_first_sanction = '', total_sanctions = '' } = req.form.values
+    const answers = req.sessionModel.get('answers') || {}
+    const { date_first_sanction = '', total_sanctions = '' } = answers
     const offenderDob = req.session?.assessment?.subject?.dob
+
     req.sessionModel.options.fields = customValidations(
       req.sessionModel.options.fields,
       offenderDob,
       date_first_sanction,
       total_sanctions,
     )
+
     super.validateFields(req, res, next)
   }
 
-  validate(req, res, next) {
-    super.validate(req, res, next)
-  }
-
   async saveValues(req, res, next) {
-    const { user, body: answers } = req
+    const { user } = req
+    const answers = answerDtoFrom(req.sessionModel.get('answers'))
 
     try {
-      await assembleDates(req, res, () => {})
       const [ok, response] = await postAnswers(
         req.session?.assessment?.uuid,
         'current',
@@ -72,7 +293,7 @@ class SaveAndContinue extends Controller {
       }
       // errors returned from OASys
       if (response.status === 422) {
-        const [validationErrors, errorSummary] = formatValidationErrors(response.errors, response.pageErrors)
+        const { validationErrors, errorSummary } = pageValidationErrorsFrom(response.errors, response.pageErrors)
         req.errors = validationErrors
         req.errorSummary = errorSummary
         // todo: add OASys errors to page and redisplay
--- app/rsr/controllers/saveAndContinue.test.js
+++ app/rsr/controllers/saveAndContinue.test.js
@@ -1,4 +1,4 @@
-const SaveAndContinue = require('./saveAndContinue')
+const SaveAndContinueController = require('./saveAndContinue')
 const { postAnswers } = require('../../../common/data/hmppsAssessmentApi')
 
 jest.mock('../../../common/data/hmppsAssessmentApi')
@@ -7,7 +7,7 @@ let req
 const user = { token: 'mytoken', id: '1' }
 const episodeUuid = '22222222-2222-2222-2222-222222222222'
 
-const saveFunction = new SaveAndContinue({
+const controller = new SaveAndContinueController({
   route: 'test-route',
 })
 
@@ -29,6 +29,7 @@ describe('post answers', () => {
         assessment: { uuid: 'test-assessment-id' },
       },
       form: {
+        fields: { date_first_sanction: { type: 'date' } },
         values: {},
       },
     }
@@ -36,60 +37,142 @@ describe('post answers', () => {
     res.render.mockReset()
   })
 
-  it('should save the answers', async () => {
-    postAnswers.mockResolvedValue([true, { episodeUuid }])
+  describe('process', () => {
+    it('should combine date fields', async () => {
+      req.form = {
+        values: {
+          'date_first_sanction-day': '2',
+          'date_first_sanction-month': '9',
+          'date_first_sanction-year': '2018',
+          age_first_conviction: '3',
+          total_sanctions: '2',
+        },
+      }
 
-    req.body = {
-      'date_first_sanction-day': '2',
-      'date_first_sanction-month': '9',
-      'date_first_sanction-year': '2018',
-      age_first_conviction: '3',
-      total_sanctions: '2',
-    }
-    await saveFunction.saveValues(req, res, () => {})
+      await controller.process(req, res, () => {})
 
-    expect(postAnswers).toHaveBeenCalledWith(
-      'test-assessment-id',
-      'current',
-      {
-        answers: {
+      expect(req.form.values).toEqual({
+        date_first_sanction: '2018-09-02',
+        age_first_conviction: '3',
+        total_sanctions: '2',
+      })
+    })
+
+    it('should return null when the date has no day component', async () => {
+      req.form = {
+        values: {
+          'date_first_sanction-month': '9',
+          'date_first_sanction-year': '2018',
+          age_first_conviction: '3',
+          total_sanctions: '2',
+        },
+      }
+
+      await controller.process(req, res, () => {})
+
+      expect(req.form.values).toEqual({
+        date_first_sanction: null,
+        age_first_conviction: '3',
+        total_sanctions: '2',
+      })
+    })
+
+    it('should return null when the date has no month component', async () => {
+      req.form = {
+        values: {
+          'date_first_sanction-day': '2',
+          'date_first_sanction-year': '2018',
+          age_first_conviction: '3',
+          total_sanctions: '2',
+        },
+      }
+
+      await controller.process(req, res, () => {})
+
+      expect(req.form.values).toEqual({
+        date_first_sanction: null,
+        age_first_conviction: '3',
+        total_sanctions: '2',
+      })
+    })
+
+    it('should return null when the date has no year component', async () => {
+      req.form = {
+        values: {
+          'date_first_sanction-day': '2',
+          'date_first_sanction-month': '9',
+          age_first_conviction: '3',
+          total_sanctions: '2',
+        },
+      }
+
+      await controller.process(req, res, () => {})
+
+      expect(req.form.values).toEqual({
+        date_first_sanction: null,
+        age_first_conviction: '3',
+        total_sanctions: '2',
+      })
+    })
+  })
+
+  describe('saveValues', () => {
+    it('should save the answers', async () => {
+      postAnswers.mockResolvedValue([true, { episodeUuid }])
+
+      req.form = {
+        values: {
           date_first_sanction: '2018-09-02',
           age_first_conviction: '3',
           total_sanctions: '2',
         },
-      },
-      user.token,
-      user.id,
-    )
-  })
+      }
 
-  it('renders an error when there are OASys validation errors', async () => {
-    postAnswers.mockResolvedValue([false, { status: 422, reason: 'OASYS_VALIDATION' }])
+      await controller.saveValues(req, res, () => {})
 
-    await saveFunction.saveValues(req, res, () => {})
+      expect(postAnswers).toHaveBeenCalledWith(
+        'test-assessment-id',
+        'current',
+        {
+          answers: {
+            date_first_sanction: '2018-09-02',
+            age_first_conviction: '3',
+            total_sanctions: '2',
+          },
+        },
+        user.token,
+        user.id,
+      )
+    })
 
-    const theError = 'Something went wrong'
+    it('renders an error when there are OASys validation errors', async () => {
+      postAnswers.mockResolvedValue([false, { status: 422, reason: 'OASYS_VALIDATION' }])
 
-    expect(res.render).toHaveBeenCalledWith('app/error', { subHeading: theError })
-  })
+      await controller.saveValues(req, res, () => {})
 
-  it('renders an error when the user does not have permission to update the assessment', async () => {
-    postAnswers.mockResolvedValue([false, { status: 403, reason: 'OASYS_PERMISSION' }])
+      const theError = 'Something went wrong'
 
-    await saveFunction.saveValues(req, res, () => {})
+      expect(res.render).toHaveBeenCalledWith('app/error', { subHeading: theError })
+    })
 
-    const theError =
-      'You do not have permission to update this type of assessment. Speak to your manager and ask them to request a change to your level of authorisation.'
+    it('renders an error when the user does not have permission to update the assessment', async () => {
+      postAnswers.mockResolvedValue([false, { status: 403, reason: 'OASYS_PERMISSION' }])
 
-    expect(res.render).toHaveBeenCalledWith('app/error', { subHeading: theError })
-  })
+      await controller.saveValues(req, res, () => {})
 
-  it('should display an error if answer saving fails', async () => {
-    const theError = new Error('Error message')
-    postAnswers.mockRejectedValue(theError)
+      const theError =
+        'You do not have permission to update this type of assessment. Speak to your manager and ask them to request a change to your level of authorisation.'
 
-    await saveFunction.saveValues(req, res, () => {})
+      expect(res.render).toHaveBeenCalledWith('app/error', { subHeading: theError })
+    })
 
-    expect(res.render).toHaveBeenCalledWith(`app/error`, { error: theError })
+    it('should display an error if answer saving fails', async () => {
+      const theError = new Error('Error message')
+      postAnswers.mockRejectedValue(theError)
+
+      await controller.saveValues(req, res, () => {})
+
+      expect(res.render).toHaveBeenCalledWith(`app/error`, { error: theError })
+    })
   })
 })
