3f2b099a4761c31587fc94c10f718bb2cf9ba1f5
Lucas Cairns <lucas.cairns@gmail.com>
2021-10-14 17:26:22 +0100

    Split out controllers for UPW

--- app/upw/controllers/taskList.js
+++ app/upw/controllers/taskList.js
@@ -1,17 +1,14 @@
-const { Controller } = require('hmpo-form-wizard')
+const BaseController = require('../../common/controllers/baseController')
 const { getTaskList } = require('./taskList.utils')
 
-class TaskList extends Controller {
+class TaskList extends BaseController {
   locals(req, res, next) {
-    res.locals.csrfToken = res.locals['csrf-token'] // TODO: move this to a BaseController class
-    res.locals.assessment = req.session.assessment
-
     res.locals.pageDescription =
       'Most of the questions in this assessment must be answered, but some are optional and are marked as such.'
 
     const journeyName = req.form?.options?.journeyName || ''
     const steps = req.form?.options?.steps || {}
-    const answers = req.form?.values || {}
+    const answers = req.sessionModel.get('answers') || {}
 
     res.locals.taskListItems = getTaskList(`/${journeyName}`, steps, answers)
 
