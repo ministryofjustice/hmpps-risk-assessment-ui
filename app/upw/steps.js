9a5d5032723ce53ad40c7affa4bdcd1bc27a98cf
Lucas Cairns <lucas.cairns@gmail.com>
2021-10-28 16:46:39 +0100

    Add declaration and confirmation pages

--- app/upw/steps.js
+++ app/upw/steps.js
@@ -3,6 +3,7 @@ const TaskList = require('./controllers/taskList')
 const BaseSaveAndContinue = require('../common/controllers/saveAndContinue')
 const SaveAndContinue = require('./controllers/saveAndContinue')
 const ConvertPdf = require('./controllers/convertPdf')
+const BaseController = require('../common/controllers/baseController')
 
 module.exports = {
   '/start': {
@@ -17,6 +18,7 @@ module.exports = {
     pageTitle: 'Community payback assessment',
     controller: TaskList,
     template: `${__dirname}/templates/taskList`,
+    next: 'confirmation',
   },
   '/individuals-details': {
     pageTitle: "Individual's details",
@@ -281,10 +283,10 @@ module.exports = {
     fields: ['upw_male_female_clothing', 'upw_waterproof_clothing', 'upw_footwear_size', 'upw_equipment_complete'],
   },
   '/pdf-preview-and-declaration': {
-    pageTitle: 'PDF preview and declaration',
+    pageTitle: 'Declaration',
     controller: BaseSaveAndContinue,
-    template: `${__dirname}/templates/default`,
-    fields: ['declaration'],
+    template: `${__dirname}/templates/declaration`,
+    fields: ['upw_declaration_confirmation'],
     next: 'task-list',
   },
   '/pdf-preview': {
@@ -300,5 +302,10 @@ module.exports = {
     noPost: true,
     template: `${__dirname}/templates/default.njk`,
     next: 'pdf-preview-and-declaration',
+  },
+  '/confirmation': {
+    pageTitle: 'Confirmation',
+    noPost: true,
+    template: `${__dirname}/templates/confirmation.njk`,
   },
 }
