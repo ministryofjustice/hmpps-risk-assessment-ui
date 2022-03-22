737f10fe11707de13947226fdb4febb71ab170de
yasmin jones <yasmin.jones@digital.justice.gov.uk>
2022-03-22 14:50:40 +0000

    phone and mobile fields now working

--- app/upw/controllers/editContactDetailsSaveAndContinue.js
+++ app/upw/controllers/editContactDetailsSaveAndContinue.js
@@ -6,7 +6,7 @@ class SaveAndContinue extends upwSaveAndContinue {
     // getting the value of the fields we are interested in
     const {
       contact_phone_number = '',
-      contact_mobile_number = '',
+      contact_mobile_phone_number = '',
       contact_address_building_name = '',
       contact_address_house_number = '',
     } = req.form.values
@@ -14,7 +14,7 @@ class SaveAndContinue extends upwSaveAndContinue {
     req.form.options.fields = customValidationsEditContactDetails(
       req.form.options.fields,
       contact_phone_number,
-      contact_mobile_number,
+      contact_mobile_phone_number,
       contact_address_building_name,
       contact_address_house_number,
     )
