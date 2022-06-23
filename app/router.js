17ab85473053bc4f06a05c77ae641ba558d782e3
Paul Coupe <paul.coupe@digital.justice.gov.uk>
2022-06-23 16:03:28 +0100

    more updated deps

--- app/router.js
+++ app/router.js
@@ -55,20 +55,6 @@ const assessmentUrl = `/${devAssessmentId}/questiongroup/ROSH/summary`
 
 // Export
 module.exports = (app) => {
-  // app.get('/health', (req, res, next) => {
-  //   const healthService = healthCheckFactory({ name: 'sentencePlanning', config: sentencePlanning })
-  //   healthService((err, result) => {
-  //     if (err) {
-  //       return next(err)
-  //     }
-  //     if (!result.healthy) {
-  //       res.status(503)
-  //     }
-  //     res.json(result)
-  //     return result
-  //   })
-  // })
-
   app.get('/health', (req, res, next) => {
     res.status(200).send({
       healthy: true,
--- test-helpers/setup.js
+++ test-helpers/setup.js
@@ -1,1 +1,5 @@
+afterAll(async () => {
+  // eslint-disable-next-line no-promise-executor-return
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+})
 jest.mock('redis', () => jest.requireActual('redis-mock'))
