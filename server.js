09406dfcebd90eaf4b4504ae458efa14a1ddbbc6
Yasmin-jones <90312885+Yasmin-jones@users.noreply.github.com>
2023-05-16 10:56:36 +0100

    Add and set up  MAINTENANCE_PAGE_TEXT env variable

--- server.js
+++ server.js
@@ -121,9 +121,10 @@ async function initialiseGlobalMiddleware(app) {
     next()
   })
 
-  if (config.displayMaintenancePage) {
+  if (config.displayMaintenancePage && config.maintenancePageText) {
     logger.info('Maintenance page enabled')
     app.get('*', (req, res) => {
+      res.locals.maintenancePageText = config.maintenancePageText
       return res.render('common/templates/maintenance-page.njk')
     })
   }
