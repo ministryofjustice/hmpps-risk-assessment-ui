ccb4244ebc959732965179701b48de390667c5ec
Lucas Cairns <lucas.cairns@gmail.com>
2025-01-24 12:42:03 +0000

    Revert login step

--- cypress/integration/features/UPW/UPWLimitedAccessOffender.feature
+++ cypress/integration/features/UPW/UPWLimitedAccessOffender.feature
@@ -5,6 +5,5 @@ Feature: Limited access offender check
   So I don't mistake it for a generic server error
 
   Scenario: The user is presented an error page when they do not have permission to access the CRN (D000403)
-    Given I am signed in
-    When I start an assessment for the CRN "D000403"
+    Given I login and navigate to UPW Start Page with CRN "D000403"
     Then I am presented with the subheading "You do not have permission"
--- cypress/support/step_definitions/UPW/UPWLimitedAccessOffender.tests.js
+++ cypress/support/step_definitions/UPW/UPWLimitedAccessOffender.tests.js
@@ -1,20 +1,4 @@
-const { Given, Then, When } = require('@badeball/cypress-cucumber-preprocessor')
-const querystring = require('querystring')
-const ArnHomePage = require('../../../integration/pages/homePage/ARNHomePage')
-const urls = require('../../../fixtures/urls.json')
-
-Given('I am signed in', () => {
-  ArnHomePage.signIn(crypto.randomUUID())
-})
-
-When('I start an assessment for the CRN {string}', (crn) => {
-  const params = querystring.encode({
-    crn,
-    assessmentType: 'UPW',
-    eventId: 1,
-  })
-  cy.visit(`${urls.startAssessment}?${params}`)
-})
+const { Then } = require('@badeball/cypress-cucumber-preprocessor')
 
 Then('I am presented with the subheading {string}', (errorMessage) => {
   cy.contains('h2', errorMessage)
