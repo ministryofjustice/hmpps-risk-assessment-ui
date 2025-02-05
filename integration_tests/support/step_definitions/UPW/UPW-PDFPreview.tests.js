d66d9c6dc83786d2c84e8e2979c8a099451b083c
devkokov <dm.kokov@gmail.com>
2025-02-05 10:01:29 +0000

    Rename test steps

--- integration_tests/support/step_definitions/UPW/UPW-PDFPreview.tests.js
+++ integration_tests/support/step_definitions/UPW/UPW-PDFPreview.tests.js
@@ -2,7 +2,7 @@ const { When } = require('@badeball/cypress-cucumber-preprocessor')
 const PdfPreviewPage = require('../../../integration/pages/upwPages/pdf-preview/pdfPreviewPage')
 const Common = require('../../../integration/pages/upwPages/common/common')
 
-When('I check the visual regression for {string}', (id) => {
+When('I check {string} for visual regression', (id) => {
   cy.compareSnapshot(id)
 })
 
