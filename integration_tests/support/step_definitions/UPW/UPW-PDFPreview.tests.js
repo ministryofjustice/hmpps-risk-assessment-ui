3ab71fd9ed5610a5c96215ee706b6e64b859493f
Lucas Cairns <lucas.cairns@gmail.com>
2025-02-13 12:40:57 +0000

    Lower threshold

--- integration_tests/support/step_definitions/UPW/UPW-PDFPreview.tests.js
+++ integration_tests/support/step_definitions/UPW/UPW-PDFPreview.tests.js
@@ -3,7 +3,7 @@ const PdfPreviewPage = require('../../../integration/pages/upwPages/pdf-preview/
 const Common = require('../../../integration/pages/upwPages/common/common')
 
 When('I check {string} for visual regression', (id) => {
-  cy.compareSnapshot(id, 0.001)
+  cy.compareSnapshot(id, 0.0001)
 })
 
 When('I check {string} for visual regression, ignoring minor differences', (id) => {
