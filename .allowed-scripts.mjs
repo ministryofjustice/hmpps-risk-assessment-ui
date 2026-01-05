91c7f26a4cdff67c8caa3020843baa12c36c6d8d
Ben Howe <ben.howe1@justice.gov.uk>
2026-01-05 14:38:06 +0000

    ARN-3056: allow list linting

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -1,14 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
 
 export default configureAllowedScripts({
-   allowlist: {
-      "node_modules/@badeball/cypress-cucumber-preprocessor@23.2.1": "ALLOW",
-      "node_modules/@parcel/watcher@2.5.1": "ALLOW",
-      "node_modules/applicationinsights-native-metrics@0.0.11": "ALLOW",
-      "node_modules/cypress@15.7.0": "ALLOW",
-      "node_modules/esbuild@0.27.0": "ALLOW",
-      "node_modules/fsevents@2.3.3": "ALLOW",
-      "node_modules/tsx/node_modules/esbuild@0.25.12": "ALLOW",
-      "node_modules/unrs-resolver@1.11.1": "ALLOW",
-   },
+  allowlist: {
+    'node_modules/@badeball/cypress-cucumber-preprocessor@23.2.1': 'ALLOW',
+    'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
+    'node_modules/applicationinsights-native-metrics@0.0.11': 'ALLOW',
+    'node_modules/cypress@15.7.0': 'ALLOW',
+    'node_modules/esbuild@0.27.0': 'ALLOW',
+    'node_modules/fsevents@2.3.3': 'ALLOW',
+    'node_modules/tsx/node_modules/esbuild@0.25.12': 'ALLOW',
+    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
+  },
 })
